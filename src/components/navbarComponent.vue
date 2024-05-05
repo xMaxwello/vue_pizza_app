e959779fc170c1eee90781ef327af1ced01d1261
xMaxwello <lukas.p9199@gmail.com>
2024-05-05 21:44:22 +0200

    fixed space between buttons

--- src/components/navbarComponent.vue
+++ src/components/navbarComponent.vue
@@ -6,7 +6,7 @@
   <header class="fixed w-full h-36 z-10 flex justify-center items-center">
     <img class="w-full h-full object-cover absolute top-0 left-0 right-0 z-0" src="../assets/navbar-bg.svg" alt="header bg"/>
 
-    <div class="flex space-x-4 z-20">
+    <div class="flex justify-between items-center w-full px-5 z-20">
       <button>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z" fill="white"/>
@@ -23,6 +23,7 @@
     </div>
   </header>
 </template>
+
 
 
 
