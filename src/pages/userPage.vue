3a8f997167b4433613c1b6f1c65002636d848f5e
xMaxwello <lukas.p9199@gmail.com>
2024-05-06 14:54:24 +0200

    updated userPage

--- src/pages/userPage.vue
+++ src/pages/userPage.vue
@@ -24,7 +24,28 @@ import orderCard from "../components/cards/orderCard.vue";
     </svg>
   </div>
   <div>
-
+    <form class="mx-5 my-4 flex flex-col space-y-5">
+      <div class="flex">
+        <label for="name" class="w-32 text-mainColor text-xs leading-[18px]">Name</label>
+        <input type="text" name="name" id="name" class="w-full outline-none border-b-2 text-mainColor text-xs leading-[18px]"/>
+      </div>
+      <div class="flex">
+        <label for="email" class="w-32 text-mainColor text-xs leading-[18px]">E-Mail</label>
+        <input type="email" name="email" id="email" class="w-full outline-none border-b-2 text-mainColor text-xs leading-[18px]"/>
+      </div>
+      <div class="flex">
+        <label for="address" class="w-32 text-mainColor text-xs leading-[18px]">Adresse</label>
+        <input type="text" name="address" id="address" class="w-full outline-none border-b-2 text-mainColor text-xs leading-[18px]"/>
+      </div>
+      <div class="flex">
+        <label for="postcode" class="w-32 text-mainColor text-xs leading-[18px]">PLZ</label>
+        <input type="text" name="postcode" id="postcode" class="w-full outline-none border-b-2 text-mainColor text-xs leading-[18px]"/>
+      </div>
+      <div class="flex">
+        <label for="passwort" class="w-32 text-mainColor text-xs leading-[18px]">Passwort</label>
+        <input type="password" name="passwort" id="passwort" class="w-full outline-none border-b-2 text-mainColor text-xs leading-[18px]"/>
+      </div>
+    </form>
   </div>
 
 
